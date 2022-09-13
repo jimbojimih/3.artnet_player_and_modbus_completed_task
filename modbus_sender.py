4ca5b68739e24b3139bdfb4e5edeb1180221d2b2
Dmitry Oparin <jim_hendrix@mail.ru>
2022-09-13 19:55:46 +0500

    preSOLID

--- modbus_sender.py
+++
@@ -1,9 +0,0 @@
-from pymodbus.client.sync import ModbusTcpClient
-
-client = ModbusTcpClient('localhost', port = 502)
-client.connect()
-# 0 - false, 1 - true
-client.write_coil(0x00, 0, unit = 0x21) 
-client.write_coil(0x01, 0, unit = 0x21)
-
-
--- modbus_server.py
+++
@@ -1,4 +0,0 @@
-import modbus_server as se
-s = se.Server(port=5020)
-s.start()
-s.set_coil(1,True)
